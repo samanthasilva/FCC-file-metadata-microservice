438c944aaa6c6920118424cec340ee93a1205dee
Samantha Silva <samanthinhasilva@gmail.com>
2024-10-09 23:05:04 +0000

    app commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer'); 
+require('dotenv').config();
 
 var app = express();
-
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var upload = multer({ dest: 'uploads/' });
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Nenhum arquivo enviado' });
+  }
+  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
-
-
+// Servidor log
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Seu app está ouvindo na porta ' + port);
 });
